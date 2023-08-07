e1bbf2ef808123911e8154381b06cb0f5868e508
Bjorn Pagen <11238136+bjornpagen@users.noreply.github.com>
2023-08-07 16:19:13 -0400

    add privacy policy

--- src/handlers.rs
+++ src/handlers.rs
@@ -216,6 +216,23 @@ pub fn base_layout(
     }
 }
 
+pub async fn redirect_dubai(req: Request, _: RouteContext<()>) -> Result<Response> {
+    let baseurl = req.headers().get("host")?.ok_or("get baseurl")?;
+    let http_whitelist = vec!["localhost:8787", "127.0.0.1:8787"];
+
+    // if localhost, http, otherwise, https
+    let proto = if http_whitelist.contains(&baseurl.as_str()) {
+        "http"
+    } else {
+        "https"
+    };
+
+    let url_str = format!("{}://{}/{}", proto, baseurl, "dubai");
+    let url = Url::parse(&url_str).unwrap();
+
+    Response::redirect_with_status(url, StatusCode::FOUND.as_u16())
+}
+
 pub async fn dubai_handler(_req: Request, _ctx: RouteContext<()>) -> Result<Response> {
     let body = html! {
         section #hero {
@@ -375,19 +392,28 @@ pub async fn dubai_handler(_req: Request, _ctx: RouteContext<()>) -> Result<Resp
     Response::from_html(body.into_string())
 }
 
-pub async fn redirect_dubai(req: Request, _: RouteContext<()>) -> Result<Response> {
-    let baseurl = req.headers().get("host")?.ok_or("get baseurl")?;
-    let http_whitelist = vec!["localhost:8787", "127.0.0.1:8787"];
-
-    // if localhost, http, otherwise, https
-    let proto = if http_whitelist.contains(&baseurl.as_str()) {
-        "http"
-    } else {
-        "https"
+pub async fn privacy_handler(_req: Request, _ctx: RouteContext<()>) -> Result<Response> {
+    let body = html! {
+        section #privacy {
+            h1 { "Privacy Policy" }
+            p { "Your privacy is important to us. It is getresidence.org's policy to respect your privacy regarding any information we may collect from you across our website, https://getresidence.org, and other sites we own and operate." }
+            p { "We only ask for personal information when we truly need it to provide a service to you. We collect it by fair and lawful means, with your knowledge and consent. We also let you know why we’re collecting it and how it will be used." }
+            p { "We only retain collected information for as long as necessary to provide you with your requested service. What data we store, we’ll protect within commercially acceptable means to prevent loss and theft, as well as unauthorized access, disclosure, copying, use or modification." }
+            p { "We don’t share any personally identifying information publicly or with third-parties, except when required to by law." }
+            p { "Our website may link to external sites that are not operated by us. Please be aware that we have no control over the content and practices of these sites, and cannot accept responsibility or liability for their respective privacy policies." }
+            p { "You are free to refuse our request for your personal information, with the understanding that we may be unable to provide you with some of your desired services." }
+            p { "Your continued use of our website will be regarded as acceptance of our practices around privacy and personal information. If you have any questions about how we handle user data and personal information, feel free to contact us." }
+            p { "This policy is effective as of 1 January 2023." }
+        }
     };
 
-    let url_str = format!("{}://{}/{}", proto, baseurl, "dubai");
-    let url = Url::parse(&url_str).unwrap();
+    let body = base_layout(
+		"getresidence.org",
+		"Privacy Policy",
+		"Your privacy is important to us. It is getresidence.org's policy to respect your privacy regarding any information we may collect from you across our website, https://getresidence.org, and other sites we own and operate.",
+		"",
+		body,
+	);
 
-    Response::redirect_with_status(url, StatusCode::FOUND.as_u16())
+    Response::from_html(body.into_string())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,7 @@ async fn main(req: Request, env: Env, _ctx: Context) -> Result<Response> {
     router
         .get_async("/dubai", handlers::dubai_handler)
         .get_async("/", handlers::redirect_dubai)
+        .get_async("/privacy", handlers::privacy_handler)
         .run(req, env)
         .await
 }
