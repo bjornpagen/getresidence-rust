c219a6ff20c27dbe5c891652985215a6ad6a81cc
Bjorn Pagen <11238136+bjornpagen@users.noreply.github.com>
2023-08-12 20:39:53 -0400

    add regex for email verification

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -1,11 +1,11 @@
-use std::{fmt::Display, str::FromStr};
+use std::str::FromStr;
 
 use base64;
 use branca::Branca;
 use http::StatusCode;
 use maud::{html, PreEscaped, DOCTYPE};
+use regex::Regex;
 use serde::{Deserialize, Serialize};
-use validator::{Validate, ValidationError};
 use wasm_bindgen::JsValue;
 use worker::*;
 
@@ -202,26 +202,24 @@ impl AsRef<str> for Name {
     }
 }
 
-#[derive(Serialize, Validate, Deserialize)]
-struct Email {
-    #[validate(email, length(max = 100))]
-    val: String,
-}
+#[derive(Serialize, Deserialize)]
+struct Email(String);
 
 impl FromStr for Email {
     type Err = Error;
 
     fn from_str(email: &str) -> Result<Self> {
-        let email = email.to_owned();
-        let validated = Self { val: email };
-        validated.validate().map_err(|e| format!("{}", e))?;
-        Ok(validated)
+        ensure!(email.len() > 0, "email must not be empty");
+        ensure!(email.len() < 100, "email must be less than 100 characters");
+        let re = Regex::new(r#"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"#).unwrap();
+        ensure!(re.is_match(email), "email must be valid");
+        Ok(Self(email.to_owned()))
     }
 }
 
 impl AsRef<str> for Email {
     fn as_ref(&self) -> &str {
-        &self.val
+        &self.0
     }
 }
 
